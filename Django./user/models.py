4f9a000efab470816d61122852d2d2ce75bb3112
Seungwon Park <130428546+Thisish0pe@users.noreply.github.com>
2023-06-29 23:11:49 +0900

    models.py - User 클래스 변경 및 CustomUserManager 클래스 추가

--- Django./user/models.py
+++ Django./user/models.py
@@ -1,20 +1,40 @@
 from django.db import models
-# 인증기능에 있는 모델을 사용하겠다. 
-from django.contrib.auth.models import AbstractUser
+# 인증기능에 있는 모델을 사용하겠다.
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# 기존의 auth 의 기능을 사용할게. 
+# 기존의 auth 의 기능을 사용할게.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
-    # User 정보 테이블 / unique 는 테이블 안에서 유일하게 하나만 있어야 해 / 유저들에게 중복 허용 안 함
     email = models.EmailField(unique=True, max_length=225)
-    # 이름은 겹치는 경우가 많아요. / null: 0이 들어가도 돼/ blank: 아무 것도 안 들어가도 돼
     name = models.CharField(max_length=50, null=True, blank=True)
     password = models.CharField(max_length=50)
     registered_date = models.DateTimeField(auto_now_add=True)
 
-    #views 안에서 유니크 한 값으로 이메일 사용한다는 것을 이야기 함
     USERNAME_FIELD = 'email'
-    #중복해서 이메일을 쓰지 말라는 것
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()  # 커스텀 UserManager를 사용합니다.
 
     def __str__(self):
         return self.name
